d554613ce36cb4691952cd7527c7d5f70e3fd60b
Justin J. Chou <jjchou@lib.uci.edu>
2019-04-19 08:17:36 -0700

    march 19th

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tktlform/precache-manifest.8734006c87d565a38203b97e1416c8b7.js"
+  "/precache-manifest.61092588b4c3674b6462f1728cba7cc6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/tktlform/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
